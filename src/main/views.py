01d26cf697c0dad2cf090223e210c15f562107f3
rohit510511 <rohitjadhav5105@gmail.com>
2025-01-10 10:45:16 +0530

    landing page done

--- src/main/views.py
+++ src/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_view(request):
-    return HttpResponse("<h1>Welcome to Automax!!</h1>")
+    return render(request,"views/main.html")
