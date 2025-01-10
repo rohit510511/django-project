1a1c12838c400754a0fd5675075e551b9c648c52
rohit510511 <rohitjadhav5105@gmail.com>
2025-01-10 08:25:57 +0530

    second add

--- src/main/views.py
+++ src/main/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def landing_vie(request):
+def main_view(request):
     return HttpResponse("<h1>Welcome to Automax!!</h1>")
